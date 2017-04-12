7823e69004c69c279f423d75b3ba1f2bd36ebda8
TJMATH <tjmath_yk@163.com>
2017-04-12 22:33:03 +0800

    2017-04-12 update the nodewise lasso code

--- nodewiseLasso.r
+++ nodewiseLasso.r
@@ -3,22 +3,15 @@ cat('\f')
 rm(list = ls())
 
 library(glmnet)
+library(mvtnorm)
 
 
 
-getData <- function(n, p, beta, sigma){
+getData <- function(n, beta, Sigma, sigma = 1){
   # Get a n x p matrix as X. Then use model Y = X %*% beta + error to get Y.
   # sigma is the std of error.
-  if(n > p){
-    print('Warning: the number of observations is greater than dimension')
-  }
-  s0 <- sum(beta!=0)
-  if (s0 > p){
-    print('Warning: Dimension should be greater than s0. And we will force the dimension parameter equal to len(beta)')
-    p <- length(beta)
-  }
-  beta <- c(beta, rep(0,p - length(beta)))
-  X <- matrix(rnorm(n * p, 0, 4), n, p)
+  p <- nrow(Sigma)
+  X <- rmvnorm(n, mean = rep(0, p), sigma = Sigma)
   error <- rnorm(n, 0, sigma)
   Y <- X%*%beta + error
   return(list(X = X, Y = Y, error = error))
@@ -35,23 +28,62 @@ getThetaLasso <- function(X, Y, lambda, lambdaj){
   beta_hat <- as.vector(model$beta)
   n <- nrow(X)
   p <- ncol(X)
-  Sigma <- (t(X) %*% X)/n
+  Sigma_hat <- (t(X) %*% X)/n
   T_hat2_inv <- diag(1, p)
   C_hat <- matrix(1, p, p)
   for(i in 1:p){
     gammai <- as.vector(glmnet(X[, -i], X[, i], lambda = lambdaj, family = "gaussian")$beta)
     C_hat[i, -i] <- -gammai
-    T_hat2_inv[i, i] <- 1/(mean((X%*%t(C_hat))^2) + lambdaj*sum(abs(gammai)))
+    T_hat2_inv[i, i] <- 1/(mean((X%*% C_hat[i, ])^2) + lambdaj*sum(abs(gammai)))
   }
   Theta_lasso <- T_hat2_inv %*% C_hat
   error <- Y - X %*% beta_hat
-  # test is the difference between (Theta_lasso %*% Sigma) and I
-  test <- sum(abs(Theta_lasso %*% Sigma - diag(p)))
-  return(list(beta_hat = beta_hat, Theta_lasso = Theta_lasso, Sigma = Sigma, error_hat = error, test = test))
+  b_hat <- beta_hat + Theta_lasso %*% t(X) %*% error/n
+  l <- 1.96 * sd(error) * sqrt(diag(Theta_lasso %*% Sigma_hat %*% t(Theta_lasso) / n))
+  CI <- cbind(b_hat - l, b_hat + l)
+  return(CI)
+}
+
+main <- function(n, s0, Sigma, c, seeds, sigma = 1, iterations = 100){
+  p <- nrow(Sigma)
+  len_CI = matrix(0, iterations, p)
+  betaInCI = matrix(0, iterations, p)
+  for(i in 1:iterations){
+    set.seed(seeds[i])
+    beta <- c(runif(s0, 0, c), rep(0, p - s0))
+    MyData <- getData(n, beta, Sigma, sigma)
+    lambdas <- getLambda(MyData$X, MyData$Y, 10)
+    CI <- getThetaLasso(MyData$X, MyData$Y, lambdas$lambda, lambdas$lambdaj)
+    len_CI[i, ]  = t(CI[, 2] - CI[, 1])
+    betaInCI[i, ] = (beta > CI[, 1]) * (beta < CI[, 2])
+    print(paste(as.character(i/iterations * 100), "% completed"))
+  }
+  return(list(len_CI = len_CI, betaInCI = betaInCI))
 }
 
 
 
-MyData <- getData(40, 150, c(1, 0, 3, 2), 1)
-lambdas <- getLambda(MyData$X, MyData$Y, 10)
-results <- getThetaLasso(MyData$X, MyData$Y, lambdas$lambda, lambdas$lambdaj)
+iterations = 100
+set.seed(65536)
+seeds = sample(65536:131072, iterations + 10, replace=FALSE)
+n = 100
+p = 500
+Sigma = matrix(0, p, p)
+for (i in 1:p){
+  for (j in 1:p){
+    Sigma[i, j] = 0.9^(abs(i - j))
+  }
+}
+s0 = 3
+c = 2
+sigma = 1
+
+results <- main(n, s0, Sigma, c, seeds, sigma, iterations)
+
+Avgcov <- colMeans(results$betaInCI)
+AvgcovS0 <- mean(Avgcov[1: s0])
+AvgcovS0c <- mean(Avgcov[s0: p - s0])
+Avglen <- colMeans(results$len_CI)
+AvglenS0 <- mean(Avglen[1: s0])
+AvglenS0c <- mean(Avglen[s0: p - s0])
+Toeplitz <- list(AvgcovS0 = AvgcovS0, AvgcovS0c = AvgcovS0c, AvglenS0 = AvglenS0, AvglenS0c = AvglenS0c)
