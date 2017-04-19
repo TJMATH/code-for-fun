295aab591ac31244dc2c79f0147e99ab4fcaf202
TJMATH <tjmath_yk@163.com>
2017-04-19 21:10:42 +0800

    2017-04-19 update nodewise lasso

--- nodewiseLasso.r
+++ nodewiseLasso.r
@@ -9,7 +9,7 @@ library(mvtnorm)
 
 getData <- function(n, beta, Sigma, sigma = 1){
   # Get a n x p matrix as X. Then use model Y = X %*% beta + error to get Y.
-  # sigma is the std of error.
+  # sigma is the std of error. And Sigma is the cov matrix of X.
   p <- nrow(Sigma)
   X <- rmvnorm(n, mean = rep(0, p), sigma = Sigma)
   error <- rnorm(n, 0, sigma)
@@ -18,32 +18,23 @@ getData <- function(n, beta, Sigma, sigma = 1){
 }
 
 getLambda <- function(X, Y, nfolds){
-  # lambda <- cv.glmnet(X, Y, nfolds = nfolds, family = "gaussian", intercept = FALSE)$lambda.min
   lambdaj <- cv.glmnet(X[, -1], X[, 1], nfolds = nfolds)$lambda.min
   return(lambdaj)
 }
 
 getThetaLasso <- function(X, Y, lambdaj){
-  # model <- glmnet(X, Y, lambda = lambda, family = "gaussian", intercept = FALSE)
-  # beta_hat <- as.vector(model$beta)
-  # n <- nrow(X)
   p <- ncol(X)
-  # Sigma_hat <- (t(X) %*% X)/n
   T_hat2_inv <- diag(1, p)
   tau_tilde <- diag(1, p)
   C_hat <- matrix(1, p, p)
   for(i in 1:p){
     gammai <- as.vector(glmnet(X[, -i], X[, i], lambda = lambdaj, family = "gaussian")$beta)
     C_hat[i, -i] <- -gammai
-    tau_tilde[i, i] <- mean((X%*% C_hat[i, ])^2)
+    tau_tilde[i, i] <- mean((X %*% C_hat[i, ])^2)
     T_hat2_inv[i, i] <- 1/(tau_tilde[i, i] + lambdaj*sum(abs(gammai)))
   }
   Theta_lasso <- T_hat2_inv %*% C_hat
   Omega_diag_hat <- diag(tau_tilde * T_hat2_inv * T_hat2_inv)
-  # error_hat <- Y - X %*% beta_hat
-  # b_hat <- beta_hat + Theta_lasso %*% t(X) %*% error_hat/n
-  # l <- 1.96 * sd(error_hat) * sqrt(diag(Theta_lasso %*% Sigma_hat %*% t(Theta_lasso)) / n)
-  # CI <- cbind(b_hat - l, b_hat + l)
   return(list(Theta_lasso = Theta_lasso, Omega_diag_hat = Omega_diag_hat))
 }
 
@@ -54,27 +45,25 @@ getsigma <- function(X, Y){
   n = length(Y)
   iter = 0
   error = 1
-  sigma_old = 0.5 # why 0.5?
+  sigma_old = -1 
+  # why 0.5? If the sigma is between 0.499 and 0.501 the loop will be broken at the first time. 
+  # So I think the initialization should be a negative number, like -1.
   while(error > 0.01 & iter < 20){
     resid = Y - X %*% beta_hat_old
     sigma_new = sd(resid)
-    lambda_new = 2 * sqrt(2 * log(p) / n) * sigma_new
+    lambda_new =  lambda * sigma_new
+    # if set lambda_new = 2Msqrt(2(t^2 + log(p))/n) * sigma, there will be a large different between b_hat and beta,
+    # also, it will penal so heavily that all beta_hatj will be zero.
+    # So how to choose a suitable lambda_{0}?
     beta_hat_new = glmnet(X, Y, lambda = lambda_new, family = "gaussian")$beta
     err1 = max(abs(beta_hat_new - beta_hat_old))
     err2 = abs(sigma_new - sigma_old)
     error = max(err1, err2)
-    print(error)
-    print(sigma_new)
-    print(beta_hat_new[1:3])
     beta_hat_old = beta_hat_new
     sigma_old = sigma_new
     iter = iter + 1
   }
   beta_hat_new <<- as.vector(beta_hat_new)
-  print("===========================")
-  print(lambda_new)
-  print(beta_hat_new[1:4])
-  print("===========================")
   return(list(sigma_hat = sigma_new, lambda_hat = lambda_new, beta_hat = beta_hat_new, error_hat = resid))
 }
 
@@ -87,23 +76,42 @@ main <- function(n, s0, Sigma, c, seeds, sigma = 1, iterations = 100){
   for(i in 1:iterations){
     set.seed(seeds[i])
     beta <- c(runif(s0, 0, c), rep(0, p - s0))
+    print("=================================")
+    print(beta[1:4])
     MyData <- getData(n, beta, Sigma, sigma)
     lambdaj <- getLambda(MyData$X, MyData$Y, 10)
     ThetaAndOmega <- getThetaLasso(MyData$X, MyData$Y, lambdaj)
     parameters <- getsigma(MyData$X, MyData$Y)
     b_hat <- parameters$beta_hat + ThetaAndOmega$Theta_lasso %*% t(MyData$X) %*% parameters$error_hat/n
+    print(b_hat[1:4])
+    
+    diff <- (b_hat - beta)
+    print("######")
+    print(max(abs(diff)))
+    print(sum(abs(diff) > 0.4))
+    
+    sigma_hat <- sd(MyData$Y - MyData$X %*% b_hat)
+    print("-----stds:----")
+    print(parameters$sigma_hat)
+    print(sigma_hat)
+    
+    # Which estimation of sigma should be used to get the length of conference regions? 
+    # Another problem is that, although the b_hat is a non-bias estimation, its std error is much larger than beta_hat
+    
     l <- 1.96 * parameters$sigma_hat * sqrt(ThetaAndOmega$Omega_diag_hat / n)
     CI <<- cbind(b_hat - l, b_hat + l)
     len_CI[i, ]  <- t(CI[, 2] - CI[, 1])
     betaInCI[i, ] <- (beta > CI[, 1]) * (beta < CI[, 2])
     print(paste(as.character(i/iterations * 100), "% completed"))
+    print("=================================")
   }
-  return(list(len_CI = len_CI, betaInCI = betaInCI))
+  return(list(CI = CI, len_CI = len_CI, betaInCI = betaInCI, sigma = sigma_hat, Omega = ThetaAndOmega$Omega_diag_hat))
 }
 
 
 
 iterations = 100
+set.seed(65536)
 seeds = sample(65536:131072, iterations + 10, replace=FALSE)
 n = 100
 p = 500
@@ -121,8 +129,9 @@ results <- main(n, s0, Sigma, c, seeds, sigma, iterations)
 
 Avgcov <- colMeans(results$betaInCI)
 AvgcovS0 <- mean(Avgcov[1: s0])
-AvgcovS0c <- mean(Avgcov[s0: p - s0])
+AvgcovS0c <- mean(Avgcov[s0 + 1: p - s0])
 Avglen <- colMeans(results$len_CI)
+hist(results$len_CI[, 1])
 AvglenS0 <- mean(Avglen[1: s0])
-AvglenS0c <- mean(Avglen[s0: p - s0])
+AvglenS0c <- mean(Avglen[s0 + 1: p - s0])
 Toeplitz <- list(AvgcovS0 = AvgcovS0, AvgcovS0c = AvgcovS0c, AvglenS0 = AvglenS0, AvglenS0c = AvglenS0c)
